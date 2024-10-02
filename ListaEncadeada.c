cfda41de36c8f61fed0bea5c4b1c54d1b16ae3ec
kaio76 <42898648+kaio76@users.noreply.github.com>
2024-10-02 02:40:21 -0300

    Update ListaEncadeada.c

--- ListaEncadeada.c
+++ ListaEncadeada.c
@@ -2,171 +2,124 @@
 #include <stdlib.h>
 #include <string.h>
 
-int cont = 0;
-
-typedef struct Tarefa {
-    int id;
+typedef struct tarefa {
+    int valor;
     char descricao[256];
-    struct Tarefa* next;
-    struct Tarefa* prev;
+    struct tarefa *next;
+    struct tarefa *prev;
 } Tarefa;
 
-
-typedef struct ListaDeTarefas {
-    Tarefa* head;
-    Tarefa* tail;
-} ListaDeTarefas;
-
-ListaDeTarefas* CriarListaDeTarefas() {
-    ListaDeTarefas* lista = (ListaDeTarefas*)malloc(sizeof(ListaDeTarefas));
-    lista->head = NULL;
-    lista->tail = NULL;
-    return lista;
-}
-
-
-void InserirTarefa(ListaDeTarefas* lista, int id, const char* descricao) {
-    Tarefa* novaTarefa = (Tarefa*)malloc(sizeof(Tarefa));
-    novaTarefa->id = id;
-    strncpy(novaTarefa->descricao, descricao, 256);
-    novaTarefa->next = NULL;
-    novaTarefa->prev = lista->tail;
-
-    if (lista->tail) {
-        lista->tail->next = novaTarefa;
-    } else {
-        lista->head = novaTarefa;
-    }
-    lista->tail = novaTarefa;
-    cont++;
-}
-
-
-void removerTarefa(ListaDeTarefas* lista, int id) {
-    Tarefa* tarefaAtual = lista->head;
-
-    while (tarefaAtual) {
-        if (tarefaAtual->id == id) {
-            if (tarefaAtual->prev) {
-                tarefaAtual->prev->next = tarefaAtual->next;
-            } else {
-                lista->head = tarefaAtual->next; 
-            }
-            if (tarefaAtual->next) {
-                tarefaAtual->next->prev = tarefaAtual->prev;
-            } else {
-                lista->tail = tarefaAtual->prev;
-            }
-            free(tarefaAtual);
-            return;
+void InserirTarefa(Tarefa **lista, int id, const char* descricao) {
+    Tarefa *novo = malloc(sizeof(Tarefa));
+    if(novo) {
+        novo->valor = id;
+        strncpy(novo->descricao, descricao, 256);
+        novo->next = *lista;
+        novo->prev = NULL;
+        if(*lista) {
+            (*lista)->prev = novo;
         }
-        tarefaAtual = tarefaAtual->next;
-    }
-}
-
-
-Tarefa* encontrarTarefa(ListaDeTarefas* lista, int id) {
-    Tarefa* tarefaAtual = lista->head;
-
-    while (tarefaAtual) {
-        if (tarefaAtual->id == id) {
-            return tarefaAtual;
-        }
-        tarefaAtual = tarefaAtual->next;
-    }
-    return NULL;
-}
-
-
-void imprimirTarefa(Tarefa * tarefas) {
-    if (tarefas) {
-        printf("ID: %d, Descricao: %s\n", tarefas->id, tarefas->descricao);
+        *lista = novo;
     } else {
-        printf("Tarefa não encontrada.\n");
+        printf("Erro ao alocar a memória\n");
     }
 }
 
+Tarefa* buscarTarefa(Tarefa **lista, int id) {
+    Tarefa *aux, *tarefa = NULL;
+    aux = *lista;
 
-void ImprimirTodasAsTarefas(ListaDeTarefas* lista) { 
-    Tarefa* tarefaAtual = lista->head; 
-    if (!tarefaAtual) { 
-        printf("Não tem tarefas na lista.\n");
-        return;
-    } 
-    printf("Todas as tarefas:\n"); 
-    while (tarefaAtual) {
-        imprimirTarefa(tarefaAtual);
-        tarefaAtual = tarefaAtual->next; 
-    }  
+    while(aux && aux->valor != id) {
+        aux = aux->next;
+    }
+    if(aux) {
+        tarefa = aux;
+    }
+    return tarefa;
 }
 
+Tarefa* remover(Tarefa **lista, int id) {
+    Tarefa *aux = NULL;
+    Tarefa *remover = NULL;
+    if(*lista) {
+        if((*lista)->valor == id) {
+            remover = *lista;
+            *lista = remover->next;
+            if(*lista) {
+                (*lista)->prev = NULL;
+            }
+        } else {
+            aux = *lista;
+            while(aux->next && aux->next->valor != id) {
+                aux = aux->next;
+            }
+            if(aux->next) {
+                remover = aux->next;
+                aux->next = remover->next;
+                if(aux->next) {
+                    aux->next->prev = aux;
+                }
+            }
+        }
+    }
+    return remover;
+}
+
+void imprimirTarefas(Tarefa *tarefa) {
+    printf("\n TAREFAS:\n\n");
+    while(tarefa) {
+        printf("ID: %d, Descricao: %s\n", tarefa->valor, tarefa->descricao);
+        tarefa = tarefa->next;
+    }
+    printf("\n");
+}
 
 int main() {
-    ListaDeTarefas* minhaTarefa = CriarListaDeTarefas();
-    int option = 0;
-
-    while (1) { 
-        printf("GERENCIADOR DE TAREFAS\n\n");
-        printf("1 - INSERIR UMA TAREFA\n");
-        printf("2 - REMOVER UMA TAREFA\n");
-        printf("3 - ENCONTRAR UMA TAREFA PELO ID\n");
-        printf("4 - LISTAR TODAS AS TAREFAS\n");
-        printf("5 - SAIR\n");
-        printf("Escolha uma opção: ");
-        scanf("%d", &option);
-
-        switch (option) {
-        case 1: {
-            char str[256];  
-            printf("\nDigite a descrição da tarefa:\n");
-            scanf(" %[^\n]s", str);
-            printf("\n**Inserindo uma Tarefa**\n"); 
-            InserirTarefa(minhaTarefa, cont, str);
-            printf("\nNova tarefa criada\n");
-            Tarefa* task = encontrarTarefa(minhaTarefa, cont - 1); 
-            printf("----------------------\n");
-            imprimirTarefa(task);
-            break;
+    Tarefa *buscar, *removido, *lista = NULL;
+    int opcao;
+    int id;
+    char str[256];
+    do {
+        printf("\nLISTA DE TAREFAS\n1 - Inserir Tarefa\n2 - Remover Tarefa\n3 - Buscar Tarefa\n4 - Listar Tarefas\n0 - Sair\n");
+        scanf("%d", &opcao);
+        switch(opcao) {
+            case 1:
+                printf("Digite o ID: \n");
+                scanf("%d", &id);
+                printf("\nDigite a descrição da tarefa:\n");
+                scanf(" %[^\n]s", str);
+                InserirTarefa(&lista, id, str);
+                printf("Tarefa Criada:\nID: %d | Descrição: %s\n", lista->valor, lista->descricao);
+                printf("\nNova tarefa criada com sucesso\n");
+                break;
+            case 2:
+                printf("Digite o ID da tarefa para remover: \n");
+                scanf("%d", &id);
+                removido = remover(&lista, id);
+                if(removido) {
+                    printf("Tarefa removida:\nID: %d | Descrição: %s\n", removido->valor, removido->descricao);
+                    free(removido);
+                } else {
+                    printf("Elemento Inexistente\n");
+                }
+                break;
+            case 3:
+                printf("Digite o ID do tarefa para busca: \n");
+                scanf("%d", &id);
+                buscar = buscarTarefa(&lista, id);
+                if(buscar) {
+                    printf("Tarefa Encontrado:\nID: %d | Descrição: %s\n", buscar->valor, buscar->descricao);
+                } else {
+                    printf("Elemento Inexistente\n");
+                }
+                break;
+            case 4:
+                imprimirTarefas(lista);
+                break;
+            default:
+                if(opcao != 0) {
+                    printf("Valor invalido");
+                }
         }
-        case 2: {
-            int remocao = 0;
-            printf("\nDigite o ID da tarefa que deseja excluir\n");
-            scanf("%d", &remocao);
-            printf("\n**Removendo uma Tarefa**\n");
-            
-            removerTarefa(minhaTarefa, remocao);
-            break;
-        }
-        case 3: {
-            int idTarefa = 0;
-            printf("\nDigite o ID da tarefa que deseja encontrar\n");
-            printf("\n**Procurando uma Tarefa**\n");
-            scanf("%d", &idTarefa);
-            printf("\n------------------------\n");
-            Tarefa* task = encontrarTarefa(minhaTarefa, idTarefa);
-            imprimirTarefa(task);
-            break;
-        }
-        case 4:
-            printf("\nMostrando todas as Tarefas\n");
-            ImprimirTodasAsTarefas(minhaTarefa);
-            break;
-        case 5: {
-            
-            Tarefa* current = minhaTarefa->head;
-            while (current) {
-                Tarefa* toFree = current;
-                current = current->next;
-                free(toFree);
-            }
-            free(minhaTarefa);
-            return 0; 
-        }
-        default:
-            printf("\nValor incorreto.\n");
-            break;
-        }
-    }
-
-    return 0;
+    } while(opcao != 0);
 }
